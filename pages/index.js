32bd10f95ecb30d830814183cdd5a708d3864f05
Fernando J. Jiménez Calsín <fjjc27.dev@gmail.com>
2022-09-26 12:49:30 -0400

    Updated index.js to use Redux

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,17 @@
 import Head from "next/head";
-import { useState } from "react";
+import { useSelector, useDispatch } from "react-redux";
 import ConvertionComponent from "../components/convertionComponent";
 import CurrencyExchangeComponent from "../components/currencyExchangeComponent";
+import { updateExchangeRate } from "../redux/slices/exchangeRateSlice";
 
 export default function Home() {
-  const [exchangeRate, setExchangeRate] = useState(3);
-  const updateExchangeRate = (newExchangeRate) => {
-    setExchangeRate(newExchangeRate);
+  const reduxDispatch = useDispatch();
+  const reduxExchangeRate = useSelector(
+    (state) => state.exchangeRateReducer.exchangeRate
+  );
+
+  const reduxUpdateExchangeRate = (newExchangeRate) => {
+    reduxDispatch(updateExchangeRate(newExchangeRate));
   };
 
   return (
@@ -19,10 +24,10 @@ export default function Home() {
 
       <main>
         <h1>Let's create a currency exchange house app!</h1>
-        <ConvertionComponent exchangeRate={exchangeRate} />
+        <ConvertionComponent exchangeRate={reduxExchangeRate} />
         <CurrencyExchangeComponent
-          exchangeRate={exchangeRate}
-          updateExchangeRate={updateExchangeRate}
+          exchangeRate={reduxExchangeRate}
+          updateExchangeRate={reduxUpdateExchangeRate}
         />
       </main>
     </div>
